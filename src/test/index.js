1eef37321794a317fc5f0c4ecaeef1cb92fae21e
satyaposwal <100473179+satyaposwal@users.noreply.github.com>
2022-04-17 23:26:48 +0530

    Update index.js

--- src/test/index.js
+++ src/test/index.js
@@ -1,1 +1,13 @@
-"test": "mocha 'src/test/**/*.js'"
+const { expect } = require('chai');
+const { agent } = require('supertest');
+const app = require('../app');
+const request = agent;
+describe('Some controller', () => {
+  it('Get request to /test returns some text', async () => {
+    const res = await request(app).get('/test');
+    const textResponse = res.body;
+    expect(res.status).to.equal(200);
+    expect(textResponse.text).to.be.a('string');
+    expect(textResponse.text).to.equal('Simple Node App Working!');
+  });
+});
