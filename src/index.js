02597b4efdb216a8ed0fab98031b1f0070a09b80
satyaposwal <100473179+satyaposwal@users.noreply.github.com>
2022-04-17 23:28:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-
+const http = require('http');
+const app = require('./app');
+// Server
+const port = process.env.PORT || 3001;
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Server running on port ${port}`));
